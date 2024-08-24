5c91b1edb474333607a706f8def75551201badbb
Stefan Asandei <asandei.stefanel@gmail.com>
2024-08-24 16:36:30 +0300

    tqdm progress bar

--- src/data/fetch.py
+++ src/data/fetch.py
@@ -6,6 +6,7 @@ Downloads from HuggingFace CDN: https://huggingface.co/datasets/commaai/comma2k1
 import os
 import requests
 import shutil
+from tqdm import tqdm
 
 from data.process import process_chunk
 from config import cfg
@@ -26,6 +27,8 @@ def download_dataset(root_path: str, num_chunks=cfg["data"]["num_chunks"]):
         chunk_dir = download_chunk(root_path, chunk_num=i)
         process_chunk(chunk_dir)
 
+    split_dataset(root_path)
+
     if cfg["data"]["log"]:
         print(f"Dataset download finished.")
 
@@ -45,11 +48,14 @@ def download_chunk(root_path: str, chunk_num: int) -> str:
 
     # download the zip
     response = requests.get(CHUNK_URL, stream=True)
-    with open(zip_name, "wb") as f:
-        for zip_chunk in response.iter_content(chunk_size=512):
+    total_size = int(response.headers.get('Content-Length', 0))
+    with open(zip_name, "wb") as f, \
+            tqdm(unit='B', total=total_size, desc=f"Chunk {chunk_num}", unit_scale=True, unit_divisor=1024) as bar:
+        for zip_chunk in response.iter_content(chunk_size=1024*1024):
             if not zip_chunk:
                 continue
-            f.write(zip_chunk)
+            size = f.write(zip_chunk)
+            bar.update(size)
 
     # extract it
     shutil.unpack_archive(zip_name, root_path)
@@ -58,3 +64,8 @@ def download_chunk(root_path: str, chunk_num: int) -> str:
     os.remove(zip_name)
 
     return chunk_dir
+
+
+def split_dataset(root_path: str):
+    print(f"Splitting {root_path} dataset.")
+    pass
