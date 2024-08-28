67710adeea58c970c2e06137fe138c147107ca3c
Stefan Asandei <asandei.stefanel@gmail.com>
2024-08-28 11:51:49 +0300

    json with comments

--- src/config.py
+++ src/config.py
@@ -8,9 +8,20 @@ import os
 cfg_path = "./config"
 cfg = {}
 
+
+class JSONWithCommentsDecoder(json.JSONDecoder):
+    def __init__(self, **kw):
+        super().__init__(**kw)
+
+    def decode(self, s: str) -> any:
+        s = '\n'.join(l for l in s.split('\n')
+                      if not l.lstrip(' ').startswith('//'))
+        return super().decode(s)
+
+
 for conf in os.listdir(cfg_path):
     conf_name = conf.split(".")[0]
     cfg[conf_name] = {}
 
     with open(f"{cfg_path}/{conf}", "r") as f:
-        cfg[conf_name] = json.load(f)
+        cfg[conf_name] = json.load(f, cls=JSONWithCommentsDecoder)
