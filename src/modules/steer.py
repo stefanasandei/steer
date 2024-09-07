789c37164b2a45b34972eab376d3b3566d8e2dbf
Stefan Asandei <asandei.stefanel@gmail.com>
2024-09-07 15:53:56 +0300

    video mamba implementation

--- src/modules/steer.py
+++ src/modules/steer.py
@@ -3,20 +3,42 @@ The main presented model. Based on the VideoMamba architecture, it levereges
 SSM's capability to process long sequence data to *steer* a vehicle.
 """
 
-import torch
-import torch.nn as nn
-from torch.nn import functional as F
-from torchvision.transforms import transforms
+from typing import Optional
 from einops import rearrange
+from torchvision.transforms import transforms
+from torch.nn import functional as F
+import torch.nn as nn
+import torch
+
+# mamba pkg raises FutureWarnings from torch usage
+import warnings
+warnings.filterwarnings("ignore")
+
+
+_is_mamba_installed = False
+try:
+    from mamba_ssm.ops.triton.layer_norm import layer_norm_fn
+    from mamba_ssm import Mamba
+
+    _is_mamba_installed = True
+except ImportError:
+    _is_mamba_installed = False
 
 
 class SteerNet(nn.Module):
-    """ """
+    """ 
+    Based on the VideoMamba (Kunchang Li et al.). Takes in a video sequence,
+    extracts 3D paches, forwards those through bidirectional Mamba blocks,
+    and returns the computed hidden state features. 
+    """
 
     def __init__(
         self,
         img_size: int,
-        n_frames: int
+        n_frames: int,
+
+        depth=24,  # from Table 1: model sizes (tiny)
+        embd_dim=192,  # from Table 1: model sizes (tiny)
     ):
         super().__init__()
 
@@ -24,20 +46,46 @@ class SteerNet(nn.Module):
         self.n_frames = n_frames
 
         self.patch_embd = VideoPatchEmbedding(
-            n_frames=n_frames, img_size=img_size)
+            n_frames=n_frames, img_size=img_size, embd_dim=embd_dim)
+
+        self.blocks = nn.ModuleList(
+            [Block(n_embd=embd_dim, layer_idx=i) for i in range(depth)])
+        self.norm = nn.LayerNorm(embd_dim)
 
     def forward(self, past_frames, past_xyz):
         """
         past_frames: (B, C, T, W, H)
-        past_xyz: (B, T, 3)
+        past_xyz: (B, T, 3) (todo)
         """
 
         # (B, T', C) -> flatten sequence of token embeddings
+        # get video patches
         patches = self.patch_embd(past_frames)
 
-        # for debug, todo
-        B = past_frames.shape[0]
-        return torch.randn(B, 128)
+        # forward through the mamba encoder
+        # same shape
+        residual = None
+        hidden_states = patches
+        for block in self.blocks:
+            hidden_states, residual = block(
+                hidden_states, residual
+            )
+
+        # layer norm
+        hidden_states = layer_norm_fn(
+            hidden_states,
+            # use the nn.LayerNorm state, but forward
+            # it using a triton kernel
+            self.norm.weight, self.norm.bias,
+            residual_in_fp32=True
+        )
+
+        # only the classification token
+        # to be passed to the av_wrapper to extract outputs
+        features = hidden_states[:, 0, :]
+
+        # (B, embd_dim)
+        return features
 
 
 class VideoPatchEmbedding(nn.Module):
@@ -114,6 +162,46 @@ class VideoPatchEmbedding(nn.Module):
         return x
 
 
+class Block(nn.Module):
+    """
+    A single block wrapping a mamba class, layer normalization and a skip connection.
+    Takes in hidden features and  returns other features (along with a residual) 
+    of the same shape. 
+    """
+
+    def __init__(self, n_embd: int, layer_idx: Optional[int] = None, norm_epsilon=1e-5):
+        super().__init__()
+
+        assert _is_mamba_installed, "Mamba is not installed. Please install it using `pip install mamba-ssm`."
+
+        self.d_model = n_embd
+        self.norm_eps = norm_epsilon
+        self.layer_idx = layer_idx
+
+        self.mamba = Mamba(d_model=n_embd, layer_idx=layer_idx)
+        self.ln = nn.LayerNorm(n_embd, eps=norm_epsilon)
+
+    def forward(self, h: torch.Tensor,
+                residual: Optional[torch.Tensor] = None) -> tuple[torch.Tensor, torch.Tensor]:
+        """
+        Takes in a hidden state (the sequence encoded from the
+        previous block) and a residual value.
+        """
+
+        # triton operation, for performance
+        h, residual = layer_norm_fn(h,
+                                    self.ln.weight, self.ln.bias,
+                                    residual,
+                                    prenorm=True, eps=self.norm_eps,
+                                    residual_in_fp32=True)
+
+        # (B, C)
+        h = self.mamba(h)
+
+        # (B, C)
+        return h, residual
+
+
 # includes resizing to the specified target
 SteerTransform = transforms.Compose([
     transforms.ToTensor(),
@@ -125,8 +213,8 @@ SteerTransform = transforms.Compose([
 # let's test the model
 if __name__ == "__main__":
     B, T, HW = 8, 30, 224
-    past_frames = torch.randn((B, 3, T, HW, HW))
-    past_xyz = torch.randn((B, T, 3))
+    past_frames = torch.randn((B, 3, T, HW, HW), device="cuda")
+    past_xyz = torch.randn((B, T, 3), device="cuda")
 
-    model = SteerNet(n_frames=T, img_size=HW)
-    model(past_frames, past_xyz)
+    model = SteerNet(n_frames=T, img_size=HW).to("cuda")
+    print(model(past_frames, past_xyz).shape)
