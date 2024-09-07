76d3f201aaff76bdd592e97e87f81bcc93fbd92f
Asandei Stefan <asandei.stefanel@gmail.com>
2024-09-07 18:02:29 +0300

    path & video encoder using mamba blocks

--- src/modules/steer.py
+++ src/modules/steer.py
@@ -12,6 +12,7 @@ import torch
 
 # mamba pkg raises FutureWarnings from torch usage
 import warnings
+
 warnings.filterwarnings("ignore")
 
 
@@ -26,19 +27,19 @@ except ImportError:
 
 
 class SteerNet(nn.Module):
-    """ 
+    """
     Based on the VideoMamba (Kunchang Li et al.). Takes in a video sequence,
     extracts 3D paches, forwards those through bidirectional Mamba blocks,
-    and returns the computed hidden state features. 
+    and returns the computed hidden state features.
     """
 
     def __init__(
         self,
         img_size: int,
         n_frames: int,
-
         depth=24,  # from Table 1: model sizes (tiny)
         embd_dim=192,  # from Table 1: model sizes (tiny)
+        drop_rate=0.1,  # from Table 6: training settings
     ):
         super().__init__()
 
@@ -46,45 +47,47 @@ class SteerNet(nn.Module):
         self.n_frames = n_frames
 
         self.patch_embd = VideoPatchEmbedding(
-            n_frames=n_frames, img_size=img_size, embd_dim=embd_dim)
+            n_frames=n_frames, img_size=img_size, embd_dim=embd_dim, drop_rate=drop_rate
+        )
 
-        self.blocks = nn.ModuleList(
-            [Block(n_embd=embd_dim, layer_idx=i) for i in range(depth)])
-        self.norm = nn.LayerNorm(embd_dim)
+        self.video_encoder = BlockList(n_layers=depth, n_hidden=embd_dim)
+        self.path_encoder = BlockList(n_layers=depth // 2, n_hidden=embd_dim)
+
+        self.head = nn.Sequential(
+            nn.Linear(embd_dim, embd_dim // 3 * 2),
+            nn.GELU(),
+            nn.Dropout(drop_rate),
+            nn.Linear(embd_dim // 3 * 2, embd_dim // 2),
+        )
 
     def forward(self, past_frames, past_xyz):
         """
         past_frames: (B, C, T, W, H)
-        past_xyz: (B, T, 3) (todo)
+        past_xyz: (B, T, 3)
+
+        returns hidden features of shape (B, embd_dim/2)
         """
 
         # (B, T', C) -> flatten sequence of token embeddings
         # get video patches
         patches = self.patch_embd(past_frames)
 
-        # forward through the mamba encoder
-        # same shape
-        residual = None
-        hidden_states = patches
-        for block in self.blocks:
-            hidden_states, residual = block(
-                hidden_states, residual
-            )
+        # (B, T', C)
+        # pass through a list of mamba blocks
+        video_features = self.video_encoder(patches)
+        path_features = self.path_encoder(patches)
 
-        # layer norm
-        hidden_states = layer_norm_fn(
-            hidden_states,
-            # use the nn.LayerNorm state, but forward
-            # it using a triton kernel
-            self.norm.weight, self.norm.bias,
-            residual_in_fp32=True
-        )
+        # add the two sets of features
+        hidden = video_features + path_features
 
         # only the classification token
         # to be passed to the av_wrapper to extract outputs
-        features = hidden_states[:, 0, :]
+        # (B, C)
+        hidden = hidden[:, 0, :]
 
-        # (B, embd_dim)
+        # (B, embd_dim/2)
+        features = self.head(hidden)
+
         return features
 
 
@@ -97,12 +100,16 @@ class VideoPatchEmbedding(nn.Module):
     The additional tokens help the SSM better learn the ordered features.
     """
 
-    def __init__(self, patch_size=16,
-                 img_size=224, img_channels=3, n_frames=3,
-                 embd_dim=192,  # from Table 1: model sizes (tiny)
-                 kernel_size=1,
-                 drop_rate=0.1  # from Table 6: training settings
-                 ):
+    def __init__(
+        self,
+        patch_size=16,
+        img_size=224,
+        img_channels=3,
+        n_frames=3,
+        embd_dim=192,  # from Table 1: model sizes (tiny)
+        kernel_size=1,
+        drop_rate=0.1,  # from Table 6: training settings
+    ):
         super().__init__()
 
         self.img_size = (img_size, img_size)
@@ -115,14 +122,14 @@ class VideoPatchEmbedding(nn.Module):
             in_channels=img_channels,
             out_channels=embd_dim,
             kernel_size=(kernel_size, patch_size, patch_size),
-            stride=(kernel_size, patch_size, patch_size)
+            stride=(kernel_size, patch_size, patch_size),
         )
 
         self.cls_token = nn.Parameter(torch.zeros(1, 1, self.embd_dim))
-        self.pos_embd = nn.Parameter(
-            torch.zeros(self.n_patches+1, self.embd_dim))
-        self.temp_embd = nn.Parameter(torch.zeros(
-            1, n_frames // kernel_size, self.embd_dim))
+        self.pos_embd = nn.Parameter(torch.zeros(self.n_patches + 1, self.embd_dim))
+        self.temp_embd = nn.Parameter(
+            torch.zeros(1, n_frames // kernel_size, self.embd_dim)
+        )
 
         self.drop = nn.Dropout(drop_rate)
 
@@ -136,11 +143,11 @@ class VideoPatchEmbedding(nn.Module):
         # convert to other shape to allow for concat
         B, C, T, H, W = x.shape
         x = x.permute(0, 2, 3, 4, 1)  # B, T, H, W, C
-        x = x.reshape(B*T, H*W, C)  # merge dims
+        x = x.reshape(B * T, H * W, C)  # merge dims
 
         # 1. classification token
         # from "lucidrains/vit-pytorch" (great repo :D)
-        cls_token = self.cls_token.expand(B*T, -1, -1)
+        cls_token = self.cls_token.expand(B * T, -1, -1)
         x = torch.cat((cls_token, x), dim=1)
         # basically (B*T, H*W+1, C); add a prefix token
 
@@ -151,9 +158,9 @@ class VideoPatchEmbedding(nn.Module):
         # 3. temporal embedding
         cls_tokens = x[:B, :1, :]  # grab first token from first frame
         x = x[:, 1:]  # exclude cls token from all frames (h*w+1 to h*w)
-        x = rearrange(x, '(b t) hw c -> (b hw) t c', b=B, t=T)
+        x = rearrange(x, "(b t) hw c -> (b hw) t c", b=B, t=T)
         x = x + self.temp_embd  # remains same shape
-        x = rearrange(x, '(b hw) t c -> b (t hw) c', b=B, t=T)
+        x = rearrange(x, "(b hw) t c -> b (t hw) c", b=B, t=T)
         # re-add the classification token
         x = torch.cat((cls_tokens, x), dim=1)
         # (B, T', C); new linear sequence of embedding tokens
@@ -165,14 +172,16 @@ class VideoPatchEmbedding(nn.Module):
 class Block(nn.Module):
     """
     A single block wrapping a mamba class, layer normalization and a skip connection.
-    Takes in hidden features and  returns other features (along with a residual) 
-    of the same shape. 
+    Takes in hidden features and  returns other features (along with a residual)
+    of the same shape.
     """
 
     def __init__(self, n_embd: int, layer_idx: Optional[int] = None, norm_epsilon=1e-5):
         super().__init__()
 
-        assert _is_mamba_installed, "Mamba is not installed. Please install it using `pip install mamba-ssm`."
+        assert (
+            _is_mamba_installed
+        ), "Mamba is not installed. Please install it using `pip install mamba-ssm`."
 
         self.d_model = n_embd
         self.norm_eps = norm_epsilon
@@ -181,34 +190,78 @@ class Block(nn.Module):
         self.mamba = Mamba(d_model=n_embd, layer_idx=layer_idx)
         self.ln = nn.LayerNorm(n_embd, eps=norm_epsilon)
 
-    def forward(self, h: torch.Tensor,
-                residual: Optional[torch.Tensor] = None) -> tuple[torch.Tensor, torch.Tensor]:
+    def forward(
+        self, h: torch.Tensor, residual: Optional[torch.Tensor] = None
+    ) -> tuple[torch.Tensor, torch.Tensor]:
         """
         Takes in a hidden state (the sequence encoded from the
         previous block) and a residual value.
         """
 
         # triton operation, for performance
-        h, residual = layer_norm_fn(h,
-                                    self.ln.weight, self.ln.bias,
-                                    residual,
-                                    prenorm=True, eps=self.norm_eps,
-                                    residual_in_fp32=True)
+        h, residual = layer_norm_fn(
+            h,
+            self.ln.weight,
+            self.ln.bias,
+            residual,
+            prenorm=True,
+            eps=self.norm_eps,
+            residual_in_fp32=True,
+        )
 
-        # (B, C)
+        # (B, T', C)
         h = self.mamba(h)
 
-        # (B, C)
+        # (B, T', C)
         return h, residual
 
 
+class BlockList(nn.Module):
+    """
+    Helper class to abstract a list of Mamba blocks. Also
+    manages residual connections.
+    """
+
+    def __init__(self, n_layers: int = 24, n_hidden: int = 192):
+        super().__init__()
+
+        self.n_layers = n_layers
+        self.n_hidden = n_hidden
+
+        self.blocks = nn.ModuleList(
+            [Block(n_embd=self.n_hidden, layer_idx=i) for i in range(self.n_layers)]
+        )
+        self.norm = nn.LayerNorm(self.n_hidden)
+
+    def forward(self, hidden):
+        # forward through the mamba encoder
+        # same shape
+        residual = None
+        for block in self.blocks:
+            hidden, residual = block(hidden, residual)
+
+        # layer norm
+        hidden = layer_norm_fn(
+            hidden,
+            # use the nn.LayerNorm state, but forward
+            # it using a triton kernel
+            self.norm.weight,
+            self.norm.bias,
+            residual_in_fp32=True,
+        )
+
+        # same shape: (B, T', C)
+        return hidden
+
+
 # includes resizing to the specified target
-SteerTransform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Resize([224, 224]),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                         std=[0.229, 0.224, 0.225]),
-])
+SteerTransform = transforms.Compose(
+    [
+        transforms.ToTensor(),
+        transforms.Resize([224, 224]),
+        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    ]
+)
 
 # let's test the model
 if __name__ == "__main__":
